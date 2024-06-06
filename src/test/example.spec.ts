4a905c3e6555e555aa29b3fd8bcc37433a4bb47b
Leonardo Lamoia Rosa <leonardotcinf@gmail.com>
2024-06-05 23:06:39 -0300

    Categorizing the tests

--- src/test/example.spec.ts
+++ src/test/example.spec.ts
@@ -1,22 +1,26 @@
-import { test, beforeAll, afterAll } from 'vitest'
+import { it, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../app'
 
-beforeAll(async () => {
-  await app.ready()
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  
+  afterAll(async () => {
+    await app.close()
+  })
+  
+  it('user can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit'
+      })
+      .expect(201)
+  })
 })
 
-afterAll(async () => {
-  await app.close()
-})
-
-test('user can create a new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit'
-    })
-    .expect(201)
-})
