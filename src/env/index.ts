eafdab86b9ea44bbbcb44e22294de8d8b0e630e2
Leonardo Lamoia Rosa <leonardotcinf@gmail.com>
2024-06-05 22:44:10 -0300

    Testando criação de transação

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'text', 'production']).default('production'),
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
   PORT: z.number().default(3333),
 })
--- src/test/example.spec.ts
+++ src/test/example.spec.ts
@@ -1,6 +1,22 @@
-import { test } from 'vitest'
+import { test, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '../app'
 
+beforeAll(async () => {
+  await app.ready()
+})
 
-test('o usuário consegue criar uma nova transação', () => {
-  
+afterAll(async () => {
+  await app.close()
+})
+
+test('user can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit'
+    })
+    .expect(201)
 })
